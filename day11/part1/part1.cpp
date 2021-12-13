eb9317e88368f504f7da89c5dace4dfbf6887cda
Vendicated <vendicated@riseup.net>
2021-12-13 21:42:03 +0100

    Improve code

--- day11/part1/part1.cpp
+++ day11/part1/part1.cpp
@@ -5,8 +5,8 @@
 #include <vector>
 
 typedef std::vector<std::vector<int>> MapType;
-void doField(MapType *map, int i, int j, int *result,
-             std::vector<std::vector<int>> *flashedFields);
+void doField(MapType &map, int i, int j, int &result,
+             std::vector<std::vector<int>> &flashedFields);
 
 int main(void) {
   std::ifstream infile("day11/input.txt");
@@ -28,7 +28,7 @@ int main(void) {
     for (int i = 0, size = map.size(); i < size; i++) {
       auto row = map[i];
       for (int j = 0, rowSize = row.size(); j < rowSize; j++) {
-        doField(&map, i, j, &result, &flashedFields);
+        doField(map, i, j, result, flashedFields);
       }
     }
 
@@ -41,16 +41,16 @@ int main(void) {
   return 0;
 }
 
-void doField(MapType *map, int i, int j, int *result,
-             std::vector<std::vector<int>> *flashedFields) {
-  if (++(*map)[i][j] > 9) {
-    for (const auto &field : *flashedFields) {
+void doField(MapType &map, int i, int j, int &result,
+             std::vector<std::vector<int>> &flashedFields) {
+  if (++map[i][j] > 9) {
+    for (const auto &field : flashedFields) {
       if (field[0] == i && field[1] == j) {
         return;
       }
     }
-    flashedFields->push_back({i, j});
-    (*result)++;
+    flashedFields.push_back({i, j});
+    result++;
     for (int m = -1; m < 2; m++) {
       for (int n = -1; n < 2; n++) {
         if (m == 0 && n == 0) {
@@ -58,8 +58,7 @@ void doField(MapType *map, int i, int j, int *result,
         }
         int i2 = i + m;
         int j2 = j + n;
-        if (i2 < 0 || j2 < 0 || i2 > map->size() - 1 ||
-            j2 > (*map)[0].size() - 1) {
+        if (i2 < 0 || j2 < 0 || i2 > map.size() - 1 || j2 > map[0].size() - 1) {
           continue;
         }
         doField(map, i2, j2, result, flashedFields);
